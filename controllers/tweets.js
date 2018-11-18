c2dc6451d38ae9bf578ab26e8697c5ff77b1ad4e
root <root@LAPTOP-1A95VQBL.localdomain>
2018-11-17 17:27:57 -0800

    hi

---
+++ controllers/tweets.js
@@ -0,0 +1,3 @@
+exports.getTweets = () => {
+    return "hello";
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const tweets = require("../controllers/tweets");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'TweetAir', tweets: tweets.getTweets() });
 });
 
 module.exports = router;
